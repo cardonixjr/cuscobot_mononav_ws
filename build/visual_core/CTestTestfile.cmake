5b27724c4fb3cfb2c3b3525efea105bee40ff877
Luciano Cardona Júnior <42819547+cardonixjr@users.noreply.github.com>
2026-01-22 17:08:39 -0300

    Delete build directory

--- build/visual_core/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cardonixjr/cuscobot_mononav_ws/src/visual_core
-# Build directory: /home/cardonixjr/cuscobot_mononav_ws/build/visual_core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
